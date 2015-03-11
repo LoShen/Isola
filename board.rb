02e3365cd89e3dd85b2eeb26c938e31ec7a5f97e
Oxedora <ed.breuille@gmail.com>
2015-03-11 17:51:53 +0100

    board : cases aux bonnes coordonnees/ cell : fonction de changement de state apres actions du joueur

--- board.rb
+++ board.rb
@@ -30,9 +30,10 @@ class Board
 			@board[incLig] = [] # On crée une colonne
 			for incCol in (0..newColumns-1) # Pour chaque combinaison de lignes et de colonnes
 				@board[incLig][incCol] = Cell.new(incLig, incCol) # On crée une nouvelle cellule avec la position correspondante
-				#print @board[incLig][incCol].color
+				@board[incLig][incCol].set_x_y(incLig, incCol)
+				@board[incLig][incCol].renvoie
 			end
-			#print "\n"
+			print "\n"
 		end
 	end
 	
@@ -43,5 +44,75 @@ class Board
 		@board[i]
 	end
 
-
 end
+
+
+Gtk.init
+	window = Gtk::Window.new
+	#window.set_size_request 450, 250 # taille par defaut de la fenetre
+	window.set_window_position :center # position initiale de la fenetre a l'ecran
+	lines = 6
+	columns = 6
+	table = Gtk::Table.new(lines, columns)
+	print "\n"
+	mat = Board.new(lines, columns)
+	puts "\n"
+	for i in 0..(lines - 1)
+  	for j in 0..(columns - 1)
+  		#mat[i][j].set_x_y(i, j)
+  		#window.add(mat[i][j])
+  		#mat[i][j].renvoie
+  		table.attach(mat[i][j], j, j+1, i, i+1)
+  	end
+  end
+  
+  print "\n"
+  print "\n"
+  print "\n"
+  
+  window.add(table)
+  '''
+  $i = 0
+	$j = 0
+	$a = 0
+	mat[$i][$j].renvoie
+	for cell in table
+		cell.signal_connect("button_press_event") do
+		$b = $a
+		puts $b
+    	#mat[$i][$j].renvoie
+    end
+    $a = $a + 1
+    
+		$j = $j + 1
+		if $j >= (columns -1)
+			$j = 0
+			$i = $i + 1
+		end
+		if $i >= (lines -1)
+			$i = 0
+		end
+		
+	end
+	'''
+  """
+  for cell in window # ceci n'est pas une cellule
+		window.child.signal_connect('button_press_event') {
+    	mat[1][1].renvoie
+    }
+	end
+"""
+	
+	
+	
+	window.show_all
+
+'''
+puts mat[2][3].state
+puts mat[2][3].renvoie
+puts mat[1][3].isAccessible
+mat[1][3].state = 2
+puts mat[1][3].isAccessible
+puts mat[2][3].isAccessible
+'''
+Gtk.main
--- cell.rb
+++ cell.rb
@@ -8,45 +8,77 @@ Attributs et comportement
 des cases du plateau.
 
 Auteur : Celia Rouquairol
-Derniere modification : Fevrier 2015
+Derniere modification : Mars 2015
 """
 
+require "gtk2"
 load "state.rb"
 
-class Cell
+class Cell < Gtk::HBox # Pour permettre de passer 2 arguments en paramètres
 
-	attr_reader :x
-	attr_reader :y
-	attr_accessor :state
-	
-	def initialize(x, y, state = State::White)
-	# Créer une case à une position donnée, sa couleur par défaut est blanche
-		@x = x
-		@y = y
-		@state = state
-	end
-	
-	def state=(newState)
-		@state = newState
-	end
-	
-	def color
-	# Affiche la couleur correspondante a la valeur de la case
-		case @state
-		when 0
-			print "Invalid"
-		when 1
-			print "White"
-		when 2
-			print "Black"
-		when 3
-			print "Player"
-		end
-	end
-	
-	def isAccessible
-	# Renvoie vrai si la case est blanche, faux sinon
-		@state == 1
-	end
-	
+  attr_accessor :x
+  attr_accessor :y
+  attr_accessor :picture
+  attr_accessor :state
+  
+  def initialize(x, y)
+    # Créer une case, sa couleur par défaut est blanche
+    super # Constructeur hérité de la classe mère
+    add(Gtk::EventBox.new.add(Gtk::Image.new('./images/case_blanche.png'))) # On ajoute une image cliquable
+    @state = State::White # L'état de la case
+    @x = x # Les coordonnées de la case
+    @y = y
+    #@picture = Gtk::Image.new('./images/case_blanche.png') # deprecated style
+    signal_connect('button_press_event') do 
+      #puts x.to_s + " " + y.to_s # instruction de test, a supprimer pour la prod
+      #puts @state # instruction de test, a supprimer pour la prod
+      if game.playerList.current.pion.x == -1 # Si la coordonnée x du pion du joueur actuel (cad celui qui clique) n'est pas initialisée (ou a -1, à choisir)
+        game.playerList.current.pion.nouvellePosition(@x, @y) # alors on initialise la position du pion
+      else # Sinon le joueur est dans une phase de jeu classique
+        case game.playerList.current.etape
+        when 1 then @state = State::Player unless !game.playerList.current.seDeplacer(@self)
+          # La première étape du joueur est de déplacer son pion, pas sûre que le self fonctionne
+        when 2 then @state = State::Black unless !game.playerList.current.noircirCase(@self) 
+          # La seconde étape du joueur est de noircir une case
+        end
+        # Les fonctions doivent informer la cellule du bon/mauvais résultat pour changer l'état de la case le cas échéant
+      end
+    end
+    
+    def set_x_y(newX, newY)
+      @x = newX
+      @y = newY
+    end
+    
+    def renvoie
+      print "["
+      print @x
+      print "]["
+      print @y
+      print "] "
+    end
+    
+    def state=(newState)
+      @state = newState
+    end
+    
+    def color
+      # Affiche la couleur correspondante a la valeur de la case
+      case @state
+      when 0
+        print "Invalid"
+      when 1
+        print "White"
+      when 2
+        print "Black"
+      when 3
+        print "Player"
+      end
+    end
+    
+    def isAccessible
+      # Renvoie vrai si la case est blanche, faux sinon
+      @state == 1
+    end
+  end
 end
